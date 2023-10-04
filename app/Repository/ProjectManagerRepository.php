45847efe883f56c11a90d9bbfa2ce9bf3959429f
Roberto Funes <robertofunes98@gmail.com>
2023-10-03 23:56:41 -0600

    Update ProjectManagerRepository.php

--- app/Repository/ProjectManagerRepository.php
+++ app/Repository/ProjectManagerRepository.php
@@ -9,35 +9,6 @@ use Illuminate\Support\Collection;
 
 class ProjectManagerRepository
 {
-    public function getTaskById(int $taskId): ?Task
-    {
-        return Task::find($taskId);
-    }
-
-    public function getCommentsByTaskId(int $taskId): Collection
-    {
-        return Task::find($taskId)->comments;
-    }
-
-    public function updateTaskStatus(int $taskId, int $statusId): bool
-    {
-        $task = Task::find($taskId);
-
-        if ($task)
-        {
-            $task->status_id = $statusId;
-
-            return $task->save();
-        }
-
-        return false;
-    }
-
-    public function validateTaskStatus(int $statusId)
-    {
-        return TaskStatus::find($statusId);
-    }
-
     public function createProject(string $name, string $description)
     {
         $project = new Project();
@@ -50,51 +21,6 @@ class ProjectManagerRepository
         return $project->refresh();
     }
 
-    public function updateTask($taskId, ?string $title, ?string $details, ?int $userId, ?int $statusId, ?int $sprintId): int
-    {
-        $builder = Task::where('id', $taskId);
-
-        $updatesArray = [];
-
-        if ($title)
-        {
-            $updatesArray['title'] = $title;
-        }
-
-        if ($details)
-        {
-            $updatesArray['details'] = $details;
-        }
-
-        if ($userId)
-        {
-            $updatesArray['user_id'] = $userId;
-        }
-
-        if ($statusId)
-        {
-            $updatesArray['status_id'] = $statusId;
-        }
-
-        if ($sprintId)
-        {
-            $updatesArray['sprint_id'] = $sprintId;
-        }
-
-        return $builder->update($updatesArray);
-    }
-
-    public function deleteTask(int $taskId)
-    {
-        $task = Task::find($taskId);
-
-        if ($task)
-        {
-            return $task->delete();
-        }
-
-        return false;
-    }
 
     public function getProjectList(int $page, int $perPage)
     {
